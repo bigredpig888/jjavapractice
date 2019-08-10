292a44947f39c8a985e2276e4b25489d5900c57a
bigredpif888 <tengjian007@126.com>
2019-08-10 19:47:58 +0800

    1947

---
+++ src/chapter3/githubtest.java
@@ -0,0 +1,12 @@
+package chapter3;
+
+/**
+ * @author my
+ * @ create 2019 -08-10-19:47
+ */
+public class githubtest {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
