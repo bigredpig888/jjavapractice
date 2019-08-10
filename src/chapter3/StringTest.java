d03ef54a44a956d9946239ce5ac5d211d93322fb
bigredpif888 <tengjian007@126.com>
2019-08-10 19:21:09 +0800

    八月10

--- src/chapter3/StringTest.java
+++ src/chapter3/StringTest.java
@@ -1,12 +1,25 @@
 package chapter3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringTest {
     public static void main(String[] args)
     {
      int age=9;
      String ra="pf"+age;
+        List l= new ArrayList() ;
+
+
+
+
      System.out.println(ra);
 
+
+m();
+    }
+    public static  void  m()
+    {
 
     }
 }
---
+++ src/chapter3/T.java
@@ -0,0 +1,23 @@
+package chapter3;
+
+/**
+ * @author my
+ * @ create 2019 -08-10-11:16
+ */
+public class T {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("T.main");
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+
+        System.out.println();
+        String[] arr=new String[]{"dd","e","f","g","h","i"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf( arr[i]);
+
+        }
+
+
+    }
+}
